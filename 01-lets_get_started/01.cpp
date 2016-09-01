0e39d3fdc3e5c017be16513d9c0aebc9d7c00aea
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-01 16:06:41 +0530

    Square pgm using class

--- 01-lets_get_started/01.cpp
+++ 01-lets_get_started/01.cpp
@@ -1,5 +1,4 @@
 /* Program to illustrate drawing of a Square using Object and Class in C++ Programming */
-/*Exception Handling is applied*/
 
 #include <iostream>
 using namespace std;
@@ -12,51 +11,28 @@ class square
        void square_data(){
          cout << "Enter side of Square\n";
          cin >> side;
-         
-        if(cin)
-        {
-         try{
-             if(side < 1)
-              throw side;
-             else if(side == 1)
-              top_bottom(side);
-            else 
-             square_draw(side);
-            }
-
-         catch(int exp){
-               cout << "Bad Integer value!\n";
-               square_draw(3);
-            }
+         top_bottom();
+         if(side > 1){
+          cout << "\n";
+          left_right();
+          top_bottom();
+         }
         }
-        else
-         cout << "Sorry you entered an invalid input";
-       }
-
-       void top_bottom(int s1){
-        for(column = 0; column < s1; ++column)
+       void top_bottom(){
+          for(column = 0; column < side; ++column)
             cout << "#";
-        }
-        
-        void left_right(int s2){
-         for(row = 0; row < s2-2; ++row){
-          cout << "#";
-          for(column=0; column < s2-2; ++column)
+         }
+        void left_right(){
+          for(row = 0; row < side-2; ++row){
+            cout<< "#";
+          for(column=0; column < side-2; ++column)
             cout << " ";
           cout << "#\n";
          }
         }
-        
-        void square_draw(int s3){
-        top_bottom(s3);
-         cout << "\n";
-         left_right(s3);
-         top_bottom(s3);   
-       }
 };
-
  int main(){
       square obj;
       obj.square_data();
       return 0;
-    }
+ }
