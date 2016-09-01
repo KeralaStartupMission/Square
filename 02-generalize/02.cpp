4c55a1b4142df0948b8c9d4561e2d0868a07d23c
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-01 17:10:05 +0530

    Indentation corrected

--- 02-generalize/02.cpp
+++ 02-generalize/02.cpp
@@ -1,62 +1,74 @@
 /* Program to illustrate drawing of a Square using Object and Class in C++ Programming */
-/*Exception Handling is applied*/
+/*Exception Handling is applied for Validation*/
 
 #include <iostream>
 using namespace std;
 
 class square
 {
-    private:
-        int side,column,row;
-    public:
-       void square_data(){
-         cout << "Enter side of Square\n";
-         cin >> side;
-         
-        if(cin)
-        {
-         try{
-             if(side < 1)
-              throw side;
-             else if(side == 1)
-              top_bottom(side);
-            else 
-             square_draw(side);
-            }
-
-         catch(int exp){
-               cout << "Bad Integer value!\n";
-               square_draw(3);
-            }
-        }
-        else
-         cout << "Sorry you entered an invalid input";
-       }
-
-       void top_bottom(int s1){
-        for(column = 0; column < s1; ++column)
-            cout << "#";
-        }
-        
-        void left_right(int s2){
-         for(row = 0; row < s2-2; ++row){
-          cout << "#";
-          for(column=0; column < s2-2; ++column)
-            cout << " ";
-          cout << "#\n";
-         }
-        }
-        
-        void square_draw(int s3){
-        top_bottom(s3);
-         cout << "\n";
-         left_right(s3);
-         top_bottom(s3);   
-       }
+private:
+  int side,column,row;
+  char character;
+public:
+  void square_data()
+  {
+    cout << "Enter character to print\n";
+    cin >> character;
+    cout << "Enter side of Square\n";
+    cin >> side;
+    
+    
+    if(cin)
+    {
+      try
+      {
+        if(side < 1)
+        throw side;
+        else if(side == 1)
+        top_bottom(side);
+        else 
+        square_draw(side);
+      }
+      
+      catch(int exp)
+      {
+        cerr << "Sorry! Expected Positive Integer Value For Box Side!\n";
+        square_draw(3);
+      }
+    }
+    else
+    cerr << "Please Confirm Both Values Are Inserted or Wrong Data!";
+  }
+  
+  void top_bottom(int var1)
+  {
+    for(column = 0; column < var1; ++column)
+    cout << character;
+  }
+  
+  void left_right(int var2)
+  {
+    for(row = 0; row < var2-2; ++row)
+    {
+      cout << character;
+      for(column=0; column < var2-2; ++column)
+      cout << " ";
+      cout << character<<"\n";
+    }
+  }
+  
+  void square_draw(int var3)
+  {
+    top_bottom(var3);
+    cout << "\n";
+    left_right(var3);
+    top_bottom(var3);   
+  }
 };
 
- int main(){
-      square obj;
-      obj.square_data();
-      return 0;
-    }
+int main()
+{
+  square obj;
+  obj.square_data();
+  return 0;
+}
