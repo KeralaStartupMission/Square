8add87758d75ca12b1e4166cf4774672a904f07c
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-02 13:15:58 +0530

    Modify by passing argument through object

--- 01-lets_get_started/01.cpp
+++ 01-lets_get_started/01.cpp
@@ -3,43 +3,71 @@
 #include <iostream>
 using namespace std;
 
-class square
+class Square
 {
+
 private:
-  int side,column,row;
+  int column,row,side;
+
 public:
-  void square_data()
+  void square_generate(int temporary_variable)
   {
-    cout << "Enter side of Square\n";
-    cin >> side;
-    top_bottom();
-    if(side > 1)
+    side = temporary_variable;
+
+    if(cin)
     {
-      cout << "\n";
-      left_right();
-      top_bottom();
+      try
+      {
+        if(side < 1)
+        throw side;
+        else if(side == 1)
+        top_bottomSide(side);
+        else
+        square_draw(side);
+      }
+
+      catch(int errorVariable)
+      {
+        cerr << "Error: Expected Positive Integer Value For Box Side!\n";
+      }
     }
+    else
+    cerr << "Error: Wrong Data!" << endl;
   }
-  void top_bottom()
+
+  void top_bottomSide(int temporary_variable)
   {
-    for(column = 0; column < side; ++column)
-    cout << "#";
+    for(column = 1; column <= temporary_variable; column++)
+    cout << '#';
+    cout << "\n";
   }
-  void left_right()
+
+  void left_rightSide(int temporary_variable)
   {
-    for(row = 0; row < side-2; ++row)
+    for(row = 1; row <= temporary_variable-2; row++)
     {
-      cout<< "#";
-      for(column=0; column < side-2; ++column)
+      cout << '#';
+      for(column=1; column <= temporary_variable-2; column++)
       cout << " ";
-      cout << "#\n";
+      cout << '#';
+      cout << "\n";
     }
+  }
+  
+  void square_draw(int draw_variable)
+  {
+    top_bottomSide(draw_variable);
+    left_rightSide(draw_variable);
+    top_bottomSide(draw_variable);
   }
 };
+
 int main()
 {
-  square obj;
-  obj.square_data();
+  int side;
+  cout << "Enter side of Square Box\n";
+  cin >> side;
+  Square obj;
+  obj.square_generate(side);
   return 0;
 }
-
