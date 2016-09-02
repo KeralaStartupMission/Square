96242087f7178a1b7c4c6d2e66bf76c4c269e503
vipinkattungal <vipinkattungal@gmail.com>
2016-09-02 17:42:01 +0530

    File rewrite using mehode ,class,object

--- 01-lets_get_started/01.rb
+++ 01-lets_get_started/01.rb
@@ -1,9 +1,35 @@
-print'enter side of metrix'
-hash = gets.to_i
-print '#' * hash + "\n"
-sl = hash - 2
-val = '#' + ' ' * sl + '#'
-sl.times do
-  puts val
+class Square
+	def initilaize(char,side)
+		@side = side
+		@char = char
+		
+	end
+	def display
+	puts"enter the side of metrix"
+	@side = gets.to_i	
+	puts"enter your character"
+	print"enter a valid number"
+    end
+	def first_last_line
+	puts @char * @side 
+	end
+	def middle_line
+	repet = @side - 2
+	middle = @char + " " * repet + @char
+		#if repet < 0
+		#puts @side * char
+	#Selse
+	repet.times do puts middle 	
+	end	
 end
-print '#' * hash
+end
+S = Square.new
+  S.display
+  S.first_last_line
+  S.middle_line
+  S.first_last_line
+end
+
+
+
+
