85cb0bca6facfd7f1a2706ef16b4256c0eadaf6e
nandu507 <nsankar507@gmail.com>
2016-09-05 23:37:18 +0530

    add default variable (SYMBOL) to symplify the code when editing

--- 01-lets_get_started/01.rb
+++ 01-lets_get_started/01.rb
@@ -1,5 +1,6 @@
 # To make a empty square
-class Square 
+class Square
+  SYMBOL = '#'
   def initialize(side)
     @side = side
   end
@@ -13,19 +14,21 @@ class Square
   end
 
   private
+
   def print_first_last_line
-    print '#'* @side
+    print SYMBOL * @side
     print "\n"
   end
 
   def print_middle_lines
     @gap_length = @side - 2
-    @gap_length.times { puts '#' + ' ' * @gap_length + '#' }
+    @gap_length.times { puts SYMBOL + ' ' * @gap_length + SYMBOL }
   end
 end
 
 print 'side_length:'
 side = gets.to_i
+
 Square
   .new(side)
   .generate
