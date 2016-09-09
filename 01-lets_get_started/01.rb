382b68f4b322e5a46a0710d99850bf17b04c7d90
nandu507 <nsankar507@gmail.com>
2016-09-09 11:11:33 +0530

    add new empty lines and more simple english to make the program more readable

--- 01-lets_get_started/01.rb
+++ 01-lets_get_started/01.rb
@@ -1,14 +1,16 @@
 # To make a empty square
 class Square
   SYMBOL = '#'
+
   def initialize(side)
     @side = side
+    @gap_length = @side - 2
   end
 
   def generate
     print_first_last_line
-
     return if @side < 2
+
     print_middle_lines
     print_first_last_line
   end
@@ -21,12 +23,11 @@ class Square
   end
 
   def print_middle_lines
-    @gap_length = @side - 2
     @gap_length.times { puts SYMBOL + ' ' * @gap_length + SYMBOL }
   end
 end
 
-print 'side_length:'
+print 'side length:'
 side = gets.to_i
 
 Square
